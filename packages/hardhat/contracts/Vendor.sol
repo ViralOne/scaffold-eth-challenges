3beacffc8dde9a05a70b72347bb3299b561eac1f
Mocanu Marian <mocanu.marian@agilefreaks.com>
2022-03-20 17:35:48 +0000

    Update Vendor.sol

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,34 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  uint256 public constant tokensPerEth = 100;
+
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
-
+  
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() external payable{
+    uint256 amountOfTokensToBuy = msg.value  * tokensPerEth;
+    require(yourToken.balanceOf(msg.sender) < amountOfTokensToBuy, "Vendor doesn't have enough tokens");
+    yourToken.transfer(msg.sender, amountOfTokensToBuy);
+    emit BuyTokens(msg.sender, msg.value, amountOfTokensToBuy);
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+   function withdraw() external onlyOwner{
+    uint256 amountOfETH = yourToken.balanceOf(msg.sender) / tokensPerEth;
+    yourToken.transfer(msg.sender, amountOfETH);
+   }
 
   // ToDo: create a sellTokens() function:
+  function sellTokens(uint256 tokensToSell) external{
+    require(yourToken.balanceOf(msg.sender) >= tokensToSell, "You don't have enough tokens to sell");
+    yourToken.transferFrom(msg.sender, address(this), tokensToSell);
+  }
 
 }
